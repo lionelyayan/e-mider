49b4896a1d68ddc463b882a9e5890ac28583bd7c
lionelyayan <lionelyayan@gmail.com>
2021-12-10 02:26:39 +0700

    upload

---
+++ app/src/main/java/com/example/emider/DBLokasi.java
@@ -0,0 +1,83 @@
+package com.example.emider;
+
+import java.io.Serializable;
+
+public class DBLokasi implements Serializable {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = -151607691806704480L;
+    private long id;
+    private String lat;
+    private String lng;
+    private String nama;
+    private double latD;
+    private double lngD;
+
+    public DBLokasi()
+    {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    // Will be used by the ArrayAdapter in the ListView
+    @Override
+    public String toString() {
+        return "("+id+")"+" Nama : "+nama+" "+" ("+lat+" , "+lng+")";
+    }
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLng() {
+        return lng;
+    }
+
+    public void setLng(String lng) {
+        this.lng = lng;
+    }
+
+    public int describeContents() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public double getLatD() {
+        latD = Double.parseDouble(lat);
+        return latD;
+    }
+
+    public void setLatD(double latD) {
+        this.latD = latD;
+    }
+
+    public double getLngD() {
+        lngD = Double.parseDouble(lng);
+        return lngD;
+    }
+
+    public void setLngD(double lngD) {
+        this.lngD = lngD;
+    }
+}
---
+++ app/src/main/java/com/example/emider/MainActivity.java
@@ -0,0 +1,115 @@
+package com.example.emider;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+import static com.google.android.gms.location.LocationServices.getFusedLocationProviderClient;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    LocationManager locationManager;
+    String kordinat, lokasi;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        FusedLocationProviderClient locationClient = getFusedLocationProviderClient(this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationClient.getLastLocation()
+                .addOnSuccessListener(new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // GPS location can be null if GPS is switched off
+                        if (location != null) {
+
+
+                            Log.e("AAA",""+location.getLatitude()+", "+location.getLongitude());
+
+                            LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                            mMap.addMarker(new MarkerOptions().position(latLng).title("Saya"));
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17));
+
+                            kordinat = Double.toString(location.getLatitude()) + "," + Double.toString(location.getLongitude());
+                            Geocoder geocoder = new Geocoder(getBaseContext(), Locale.getDefault());
+
+//                            try {
+//                                List<Address> addresses = geocoder.getFromLocation(location.getLatitude(),
+//                                        location.getLongitude(), 1);
+//
+//                                lokasi = addresses.get(0).getAddressLine(0);
+//
+//                            } catch (IOException e) {
+//
+//                                Log.e("AAA", "GAGAL Mendapatkan Address");
+//                            }
+                        } else{
+                            Log.e("AAA", "not found location");
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e("AAA", "GAGAL Mendapatkan Lokasi");
+                        Toast.makeText(getBaseContext(), "Error trying to get last GPS location", Toast.LENGTH_SHORT).show();
+                        e.printStackTrace();
+
+                    }
+                });
+    }
+
+}
